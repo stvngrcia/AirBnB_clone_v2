c5606e8f5c21e5c4234b1cf63d31a518e623aa0c
stvngrcia <stevengarciaacuna@gmail.com>
2018-03-24 16:07:47 -0700

    updating base_model and city to account for database storage

--- models/base_model.py
+++ models/base_model.py
@@ -5,12 +5,20 @@
 import uuid
 from datetime import datetime
 import models
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
 
+
+Base = declarative_base()
 
 class BaseModel:
     '''
         Base class for other classes to be used for the duration.
     '''
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+
     def __init__(self, *args, **kwargs):
         '''
             Initialize public instance attributes.
@@ -19,7 +27,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+
         else:
             kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
                                                      "%Y-%m-%dT%H:%M:%S.%f")
@@ -27,7 +35,8 @@ class BaseModel:
                                                      "%Y-%m-%dT%H:%M:%S.%f")
             for key, val in kwargs.items():
                 if "__class__" not in key:
-                    setattr(self, key, val)
+                    self.key = val
+                    #setattr(self, key, val)
 
     def __str__(self):
         '''
@@ -48,6 +57,7 @@ class BaseModel:
             Update the updated_at attribute with new.
         '''
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -55,8 +65,19 @@ class BaseModel:
             Return dictionary representation of BaseModel class.
         '''
         cp_dct = dict(self.__dict__)
+        try:
+            del cp_dct['_sa_instance_state']
+        except ValueError:
+            pass
         cp_dct['__class__'] = self.__class__.__name__
         cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
 
         return (cp_dct)
+
+    def delete(self):
+        '''
+            Deletes the current instance from the storage
+                by calling the method delete.
+        '''
+        models.storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -3,11 +3,16 @@
     Implementation of the State class
 '''
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     '''
         Implementation for the State.
     '''
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete, delete-orphan",
+                          primaryjoin="City.state.id==State.id")
