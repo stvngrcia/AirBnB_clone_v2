a81ecb2126a8099a6d82714149db865964459df2
stvngrcia <stevengarciaacuna@gmail.com>
2018-03-25 14:32:29 -0700

    adding columns to user class to implement sqalchemy

--- models/user.py
+++ models/user.py
@@ -2,14 +2,15 @@
 '''
     Implementation of the User class which inherits from BaseModel
 '''
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
-
-class User(BaseModel):
+class User(BaseModel, Base):
     '''
         Definition of the User class
     '''
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
