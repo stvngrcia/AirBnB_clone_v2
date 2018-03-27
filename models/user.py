9565643caa584632830f20643fbdad552bc6973c
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-03-27 22:25:53 +0000

    Added condition to make attributes based on storage

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 '''
     Implementation of the User class which inherits from BaseModel
 '''
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -9,13 +10,19 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     '''
-        Definition of the User class
+    Definition of the User class
     '''
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user",
-                          cascade="delete")
-    reviews = relationship("Review", cascade="delete", backref="user")
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", backref="user",
+                              cascade="delete")
+        reviews = relationship("Review", cascade="delete", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
