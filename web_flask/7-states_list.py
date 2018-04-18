b0cfedc4509b1fa2d890cc19fed1ff93a5d7c721
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-18 10:31:08 -0700

    creating a list of state to be queried for a html format

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from flask import Flask
+from models import storage
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
